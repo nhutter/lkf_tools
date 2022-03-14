d5d096ffcd3a9f61af72345b56c1739c10626798
Nils Hutter <nils.hutter@awi.de>
2022-03-14 15:31:08 -0700

    initialize move of lkf_tools to python package

---
+++ lkf_tools/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+The lkf_tools python package is project to develop detection and tracking
+routines to identify deformation features (leads, pressure ridges, LKFs)
+in gridded sea-ice deformation data. The package includes statistical tools
+to analyse spatial and temporal statistics of LKFs.
+"""
+
+
+# Package Metadata
+__version__ = 0.1
+__author__ = "Nils Hutter"
+__author_email__ = "nhutter@uw.edu"
+
